8a75b8d3c160a43b96e42f6dc2c1181b6cbdc00a
sulsyh <71731748+sulsyh@users.noreply.github.com>
2023-12-31 18:00:02 +0700

    test.sh

--- test.sh
+++ test.sh
@@ -11,7 +11,7 @@ if [ -f "LuckyProxy" ]; then
     sleep 1
     echo -e "${GREEN}Getting proxy...${ENDCOLOR}"
 fi
-wget -q https://github.com/AyamNubCh/AnchProxy/raw/main/LuckyProxy
+wget -q https://github.com/sulsyh/StarAndro/raw/main/LuckyProxy
 sleep 1
 echo -e "${GREEN}Proxy Installed${ENDCOLOR}"
 echo -e "${GREEN}Execute proxy with this command: ./proxy${ENDCOLOR}"
